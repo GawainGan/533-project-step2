ca81e0ac4b08df12d6948af2e361ece968b1b64b
Gawain Gan <41322285+GawainGan@users.noreply.github.com>
2022-12-23 11:18:49 -0800

    Updated test file for stock_main

--- Test_Stock_Main.py
+++ Test_Stock_Main.py
@@ -31,6 +31,41 @@ class TestStock(unittest.TestCase): # test class
             self.assertEqual(self.n , len(self.high_price_list()))
             self.assertEqual(self.n , len(self.low_price_list()))
             self.assertEqual(self.n , 4) # it will has error
+      
+      def test_get_high_price(self):
+            stock = Stock()
+            high_prices = stock.get_high_price()
+            self.assertIsInstance(high_prices, list)
+            self.assertGreaterEqual(max(high_prices), 201)
+            self.assertLessEqual(min(high_prices), 500)
+
+      def test_get_low_price(self):
+            stock = Stock()
+            low_prices = stock.get_low_price()
+            self.assertIsInstance(low_prices, list)
+            self.assertGreaterEqual(max(low_prices), 50)
+            self.assertLessEqual(min(low_prices), 200)
+
+      def test_get_volume(self):
+            stock = Stock()
+            volumes = stock.get_volume()
+            self.assertIsInstance(volumes, list)
+            self.assertGreaterEqual(max(volumes), 1)
+            self.assertLessEqual(min(volumes), 200)
+            
+      def test_get_size(self):
+            stock = Stock()
+            size = stock.get_size()
+            self.assertIsInstance(size, int)
+            self.assertGreaterEqual(size, 0)
+      
+      def test_str(self):
+            stock = Stock()
+            stock_str = str(stock)
+            self.assertIsInstance(stock_str, str)
+            self.assertIn("high price list:", stock_str)
+            self.assertIn("low price list:", stock_str)
+            self.assertIn("volume list:", stock_str)
             
             
             
